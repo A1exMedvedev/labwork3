ae87a2f7c42427846899d4048ab3347588d49e3c
A1exMedvedev <map22032006@mail.ru>
2024-10-16 21:26:47 +0300

    Add comments to extension.js

--- extension.js
+++ extension.js
@@ -1,28 +1,24 @@
-const vscode = require('vscode');
-const translate = require('./keymap'); // Убедитесь, что путь правильный
+const vscode = require('vscode'); // Импортирует модуль vscode, который предоставляет API для работы с редактором vscode.
+const translate = require('./keymap'); // импорт модуля keymap
 
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.translateRussian', () => {
-        const editor = vscode.window.activeTextEditor;
+    let disposable = vscode.commands.registerCommand('extension.translateRussian', () => { // создаем команду extension.translateRussian
+        const editor = vscode.window.activeTextEditor; // создает редактор выделенного текста
         if (editor) {
-            const text = editor.document.getText(editor.selection);
+            const text = editor.document.getText(editor.selection); // распаковывает редактор в текст
             if (!text) {
-                vscode.window.showInformationMessage('Пожалуйста, выделите текст для перевода.');
+                vscode.window.showInformationMessage('Пожалуйста, выделите текст для перевода.'); // проверка выделен ли текст
                 return;
             }
-            const translatedText = translate(text);
-            editor.edit(editBuilder => {
-                editBuilder.replace(editor.selection, translatedText);
+            const translatedText = translate(text); // вызывает функцию перевода текста
+            editor.edit(editBuilder => { // задает редактирование редактора
+                editBuilder.replace(editor.selection, translatedText); // земняет текст на переведенный
             });
         }
     });
-
-    context.subscriptions.push(disposable);
 }
 
-function deactivate() {}
 
-module.exports = {
-    activate,
-    deactivate
+module.exports = { // экспорт функций, для их поддержки
+    activate
 };
